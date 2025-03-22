70bc162243677b118815e26f71b4a6bab12929ee
kayinsg <kayin.gayle@gmail.com>
2025-03-22 15:43:16 -0400

    test: add test for creation of spreadsheet

--- UpdateSpreadsheet/test.py
+++ UpdateSpreadsheet/test.py
@@ -19,4 +19,33 @@ class SpreadsheetTests(unittest.TestCase):
 
         self.assertEqual(monthFolderPath, "March/")
 
+
+    def testShouldCreateSpreadsheetFileRepresentingTheWeekNumberInMonthGivenCurrentDate(self):
+        class FakeSpreadsheetFileCreator(SpreadsheetFileCreator):
+            def __init__(self, fileCreator, currentDate):
+                self.fileCreator = fileCreator
+                self.currentDate = currentDate
+
+            def getWorkbook(self, spreadsheetFilePath):
+                return spreadsheetFilePath
+
+        class FakeFileCreator(FileCreator):
+            def __init__(self, parentDirectory):
+                self.parentDirectory = parentDirectory
+
+            def createSpreadsheetFile(self, filePath):
+                return filePath
+
+        def get_week_in_month(iso_date: str) -> int:
+            date = pendulum.parse(iso_date)
+            return date.week_of_month
+
+        parentDirectory = ""
+        fileCreator = FakeFileCreator(parentDirectory)
+        weekWithinMonth = get_week_in_month(self.currentDate)
+
+        spreadSheetFilePath = FakeSpreadsheetFileCreator(fileCreator, self.currentDate).createSpreadsheet()
+
+        self.assertEqual(spreadSheetFilePath, f"{parentDirectory}Week {weekWithinMonth}.xlsx")
+
 unittest.main()
