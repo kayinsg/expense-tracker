6752bb0b82552809a87a3d99dd681f08a1fdf6fa
kayinsg <kayin.gayle@gmail.com>
2025-03-25 19:21:14 -0400

    test(style): reorder namespaces

--- UpdateSpreadsheet/test.py
+++ UpdateSpreadsheet/test.py
@@ -8,41 +8,6 @@ from pandas import DataFrame
 from colour_runner.runner import ColourTextTestRunner
 
 
-class SpreadsheetWorkbookPopulationTest(unittest.TestCase):
-
-        @staticmethod
-        def firstDayOfWeek(weekDays):
-            return weekDays[0].format('dddd')
-
-        @staticmethod
-        def lastDayOfWeek(weekDays):
-            return weekDays[-1].format('dddd')
-
-        def testPreviousMonth(self):
-            dateWithPreviousMonthWeekdays = '2025-03-01'
-            weekDaysInvolvingThePreviousMonth = ['Feb.23.2025', 'Feb.24.2025', 'Feb.25.2025', 'Feb.26.2025', 'Feb.27.2025', 'Feb.28.2025', 'Mar.01.2025']
-
-            weekDaysStartingFromSunday = WeekNormalizer(dateWithPreviousMonthWeekdays).getWeekdays()
-
-            self.assertEqual(weekDaysStartingFromSunday, weekDaysInvolvingThePreviousMonth)
-
-        def testSameMonth(self):
-            dateWithSameMonthWeekdays = '2025-03-27'
-            weekDaysInvolvingOnlyTheSameMonth = ['Mar.23.2025', 'Mar.24.2025', 'Mar.25.2025', 'Mar.26.2025', 'Mar.27.2025', 'Mar.28.2025', 'Mar.29.2025']
-
-            weekDaysStartingFromSunday = WeekNormalizer(dateWithSameMonthWeekdays).getWeekdays()
-
-            self.assertEqual(weekDaysStartingFromSunday, weekDaysInvolvingOnlyTheSameMonth)
-
-        def testSucceedingMonth(self):
-            dateWithSuceedingMonthWeekdays = '2025-04-05'
-            weekDaysInvolvingTheSucceedingMonth = ['Mar.30.2025', 'Mar.31.2025', 'Apr.01.2025', 'Apr.02.2025', 'Apr.03.2025', 'Apr.04.2025', 'Apr.05.2025']
-
-            weekDaysStartingFromSunday = WeekNormalizer(dateWithSuceedingMonthWeekdays).getWeekdays()
-
-            self.assertEqual(weekDaysStartingFromSunday,weekDaysInvolvingTheSucceedingMonth)
-
-
 class SpreadsheetFileSystemTest(unittest.TestCase):
 
     currentDate = '2025-03-20'
@@ -93,6 +58,41 @@ class SpreadsheetFileSystemTest(unittest.TestCase):
         spreadsheet = FakeSpreadsheetFileCreator(fileCreator, self.currentDate).create()
 
         self.assertEqual(spreadsheet['FilePath'], f"{parentDirectory}Week {weekWithinMonth}.xlsx")
+
+
+class SpreadsheetWorkbookPopulationTest(unittest.TestCase):
+
+        @staticmethod
+        def firstDayOfWeek(weekDays):
+            return weekDays[0].format('dddd')
+
+        @staticmethod
+        def lastDayOfWeek(weekDays):
+            return weekDays[-1].format('dddd')
+
+        def testPreviousMonth(self):
+            dateWithPreviousMonthWeekdays = '2025-03-01'
+            weekDaysInvolvingThePreviousMonth = ['Feb.23.2025', 'Feb.24.2025', 'Feb.25.2025', 'Feb.26.2025', 'Feb.27.2025', 'Feb.28.2025', 'Mar.01.2025']
+
+            weekDaysStartingFromSunday = WeekNormalizer(dateWithPreviousMonthWeekdays).getWeekdays()
+
+            self.assertEqual(weekDaysStartingFromSunday, weekDaysInvolvingThePreviousMonth)
+
+        def testSameMonth(self):
+            dateWithSameMonthWeekdays = '2025-03-27'
+            weekDaysInvolvingOnlyTheSameMonth = ['Mar.23.2025', 'Mar.24.2025', 'Mar.25.2025', 'Mar.26.2025', 'Mar.27.2025', 'Mar.28.2025', 'Mar.29.2025']
+
+            weekDaysStartingFromSunday = WeekNormalizer(dateWithSameMonthWeekdays).getWeekdays()
+
+            self.assertEqual(weekDaysStartingFromSunday, weekDaysInvolvingOnlyTheSameMonth)
+
+        def testSucceedingMonth(self):
+            dateWithSuceedingMonthWeekdays = '2025-04-05'
+            weekDaysInvolvingTheSucceedingMonth = ['Mar.30.2025', 'Mar.31.2025', 'Apr.01.2025', 'Apr.02.2025', 'Apr.03.2025', 'Apr.04.2025', 'Apr.05.2025']
+
+            weekDaysStartingFromSunday = WeekNormalizer(dateWithSuceedingMonthWeekdays).getWeekdays()
+
+            self.assertEqual(weekDaysStartingFromSunday,weekDaysInvolvingTheSucceedingMonth)
 
 
 class SpreadsheetWriterTest(unittest.TestCase):
