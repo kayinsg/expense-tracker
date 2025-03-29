b000c2604284138c3d99bf646de13fd15a786f74
kayinsg <kayin.gayle@gmail.com>
2025-03-28 20:55:45 -0400

    feat: convert series & dataframe into nested list

--- UpdateSpreadsheet/SpreadsheetWriter.py
+++ UpdateSpreadsheet/SpreadsheetWriter.py
@@ -1,2 +1,25 @@
 from pandas import DataFrame
-import openpyxl
+
+def consolidateSeriesAndDataFrame(dataFrame: pandas.DataFrame, series: pandas.Series):
+    result = [ ]
+
+    dataFrameColumns =  [ ]
+    for column in dataFrame.columns.tolist():
+        dataFrameColumns.append(column)
+    result.append(dataFrameColumns)
+
+    for dataStructure in dataFrame.iterrows():
+        result.append(dataStructure[1].tolist())
+
+    seriesHeaders = [ ]
+    for index in series.index.tolist():
+        seriesHeaders.append(index)
+    result.append(seriesHeaders)
+
+    seriesData = [ ]
+    for header in seriesHeaders:
+        seriesData.append(series.loc[header].tolist())
+
+    result.append(seriesData)
+
+    return result
