6c497aa1d7b893a1984c497db9c733709bababe8
kayinsg <kayin.gayle@gmail.com>
2025-04-02 01:58:22 -0400

    refactor: inject row number dependency

--- main.py
+++
@@ -1,6 +0,0 @@
-from ExtractBudgetData.DataExtractor import DataFacade
-from UpdateSpreadsheet.Spreadsheet import Spreadsheet
-from paths import flatTextFile, spreadsheetPath
-
-data = DataFacade(flatTextFile).get("Formatted")
-Spreadsheet(spreadsheetPath).apply(data)
