ef960a11cccced184ec9dec76d0125204072b9b0
kayinsg <kayin.gayle@gmail.com>
2025-05-23 14:45:35 -0400

    file: remove old abstractions

--- GlobalDataObjects.py
+++
@@ -1,7 +0,0 @@
-from typing import NamedTuple
-from pandas import DataFrame, Series
-
-
-class Data(NamedTuple):
-    table: DataFrame
-    summary: Series
--- UpdateSpreadsheet/dataObjects.py
+++ UpdateSpreadsheet/dataObjects.py
@@ -1,11 +1,4 @@
-from pandas import DataFrame, Series
 from typing import NamedTuple
-import openpyxl
-
-class SpreadsheetDetails(NamedTuple):
-    filePath            : str
-    workbook            : openpyxl.workbook.workbook.Workbook
-    worksheet           : openpyxl.worksheet.worksheet.Worksheet
 
 class FontProfile(NamedTuple):
     name       : str
