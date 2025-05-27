8c415a4b41120547d69465eb509d8680d9de0f6f
kayinsg <kayin.gayle@gmail.com>
2025-05-26 21:00:37 -0400

    import: update module namespaces for worksheet

--- main.py
+++ main.py
@@ -1,38 +1,29 @@
-from UpdateSpreadsheet.SpreadsheetFormatter import SpreadsheetFormatter
+from UpdateSpreadsheet.WorksheetFormatter import SpreadsheetFormatter
 from paths import flatTextFile, saveDirectoryForSpreadsheets
 import pendulum
 
 from UpdateSpreadsheet.FileSystem import FileSystem
 from UpdateSpreadsheet.WorkbookPopulator import WorkbookPopulator
-from UpdateSpreadsheet.SpreadsheetWriter import WorksheetDataDepositor
+from UpdateSpreadsheet.WorksheetWriter import WorksheetDataDepositor
 from UpdateSpreadsheet.utils import DateTranslator
 
 class Spreadsheet:
     def execute(self):
-        currentIsoDate = self.getCurrentdate('iso')
-        workbook = self.createCreateSpreadsheetOnFileSystem(currentIsoDate)
-        workbookWithDateWorksheets = self.populateWorkbookWithWorksheets(workbook, currentIsoDate)
-        currentSpreadsheetDate = self.getCurrentdate('spreadsheet')
-        dataFromTextFile = self.extractDataFromTextFile()
+        currentDate = self.getCurrentdate()
+        workbook = FileSystem(saveDirectoryForSpreadsheets).setUpSpreadsheet(currentDate)
+        workbookWithDateWorksheets = WorkbookPopulator(currentDate).populate(workbook['Workbook'])
+        currentSpreadsheetDate = self.getCurrentdate()
+
         worksheet = self.insertDataInSpreadsheet(workbookWithDateWorksheets, currentSpreadsheetDate, dataFromTextFile)
         SpreadsheetFormatter(worksheet).apply()
         workbook['Workbook'].save(workbook['FilePath'])
 
-    def getCurrentdate(self, typeOfDate):
+    def getCurrentdate(self):
         date = pendulum.now().format('YYYY-MM-DD')
-        return DateTranslator(date).translateDate(typeOfDate)
+        return DateTranslator(date).convert()
 
-    def extractDataFromTextFile(self):
-        return DataFacade(flatTextFile).get("Formatted")
-
-    def createCreateSpreadsheetOnFileSystem(self, date):
-        return FileSystem(saveDirectoryForSpreadsheets).setUpSpreadsheet(date)
-
-    def populateWorkbookWithWorksheets(self, workbook, currentDate):
-        return WorkbookPopulator(currentDate).populate(workbook['Workbook']) 
-
-    def insertDataInSpreadsheet(self, workbookWithDateWorksheets, spreadsheetDate, data):
-        workbookFilledWithData = WorksheetDataDepositor(workbookWithDateWorksheets, spreadsheetDate).insert(data.table, data.summary)
+    def insertDataInSpreadsheet(self, workbookWithDateWorksheets, spreadsheetDate):
+        workbookFilledWithData = WorksheetDataDepositor(workbookWithDateWorksheets, spreadsheetDate).insert(budgetInfo)
         return workbookFilledWithData[spreadsheetDate]
 
 Spreadsheet().execute()
