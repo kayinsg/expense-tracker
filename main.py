5e073f476850283d52695e542f5ba11217aada64
kayinsg <kayin.gayle@gmail.com>
2025-05-27 02:00:23 -0400

    refactor(major): revamp entry point for spreadsheet

--- main.py
+++ main.py
@@ -1,29 +1,22 @@
-from UpdateSpreadsheet.WorksheetFormatter import SpreadsheetFormatter
-from paths import flatTextFile, saveDirectoryForSpreadsheets
-import pendulum
+from ExtractBudgetData.Cost import Cost
+from ExtractBudgetData.TextFile import TextFile
 
+from UpdateSpreadsheet.DateTranslator import getCurrentDate
 from UpdateSpreadsheet.FileSystem import FileSystem
 from UpdateSpreadsheet.WorkbookPopulator import WorkbookPopulator
+from UpdateSpreadsheet.WorksheetFormatter import SpreadsheetFormatter
 from UpdateSpreadsheet.WorksheetWriter import WorksheetDataDepositor
-from UpdateSpreadsheet.utils import DateTranslator
 
-class Spreadsheet:
-    def execute(self):
-        currentDate = self.getCurrentdate()
-        workbook = FileSystem(saveDirectoryForSpreadsheets).setUpSpreadsheet(currentDate)
-        workbookWithDateWorksheets = WorkbookPopulator(currentDate).populate(workbook['Workbook'])
-        currentSpreadsheetDate = self.getCurrentdate()
+from paths import flatTextFile, saveDirectoryForSpreadsheets
 
-        worksheet = self.insertDataInSpreadsheet(workbookWithDateWorksheets, currentSpreadsheetDate, dataFromTextFile)
-        SpreadsheetFormatter(worksheet).apply()
-        workbook['Workbook'].save(workbook['FilePath'])
+def createSpreadsheet():
+    textFileData = TextFile(flatTextFile).extractData()
+    budgetInfo = Cost(textFileData).getBudgetInfo('formatted')
+    currentDate = getCurrentDate()
+    workbook = FileSystem(saveDirectoryForSpreadsheets).setUpSpreadsheet(currentDate.iso)
+    workbookWithDateWorksheets = WorkbookPopulator(currentDate.iso).populate(workbook['Workbook'])
+    worksheet = WorksheetDataDepositor(workbookWithDateWorksheets, currentDate.spreadsheet).insert(budgetInfo)
+    SpreadsheetFormatter(worksheet[currentDate.spreadsheet]).apply()
+    workbook['Workbook'].save(workbook['FilePath'])
 
-    def getCurrentdate(self):
-        date = pendulum.now().format('YYYY-MM-DD')
-        return DateTranslator(date).convert()
-
-    def insertDataInSpreadsheet(self, workbookWithDateWorksheets, spreadsheetDate):
-        workbookFilledWithData = WorksheetDataDepositor(workbookWithDateWorksheets, spreadsheetDate).insert(budgetInfo)
-        return workbookFilledWithData[spreadsheetDate]
-
-Spreadsheet().execute()
+createSpreadsheet()
