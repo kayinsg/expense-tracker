b997c267a3be7ba3705d198e2b07d0ad046b8943
kayinsg <kayin.gayle@gmail.com>
2025-05-27 01:45:20 -0400

    refactor: rewrite date translator to be functional

--- UpdateSpreadsheet/DateTranslator.py
+++ UpdateSpreadsheet/DateTranslator.py
@@ -1,34 +1,22 @@
 import pendulum
+from typing import Callable, NamedTuple
 
+class DateFormats(NamedTuple):
+    iso: str
+    spreadsheet: str
 
-class DateTranslator:
-    def __init__(self, date):
-        # Check if the date is already in ISO format (YYYY-MM-DD)
-        try:
-            parsed = pendulum.parse(date, strict=False)
-            if parsed.format('YYYY-MM-DD') == date:
-                self.date = date
-                self.is_iso = True
-            else:
-                self.date = date
-                self.is_iso = False
-        except:
-            self.date = date
-            self.is_iso = False
+def getCurrentDate():
+    dateFromPendulum = getDateFromPendulum
+    return getCurrentDateObject(dateFromPendulum)
 
-    def translateDate(self, targetFormat):
-        if targetFormat == 'spreadsheet':
-            if self.is_iso:
-                return pendulum.parse(self.date).format('MMM.DD.YYYY')
-            return self.convertDateToSpreadsheetFormat(self.date)
-        elif targetFormat == 'iso':
-            if self.is_iso:
-                return self.date
-            return self.convertDateToPendulumFormat(self.date)
-        raise ValueError("Unknown target format")
+def getCurrentDateObject(dateProvider):
+    dateStr = dateProvider()
+    dateObj = pendulum.parse(dateStr)
 
-    def convertDateToSpreadsheetFormat(self, date):
-        return pendulum.parse(date, strict=False).format('MMM.DD.YYYY')
+    isoFormat = dateObj.format('YYYY-MM-DD')
+    spreadsheetFormat = dateObj.format('MMM.DD.YYYY')
 
-    def convertDateToPendulumFormat(self, date):
-        return pendulum.from_format(date, 'MMM.DD.YYYY').format('YYYY-MM-DD')
+    return DateFormats(iso=isoFormat, spreadsheet=spreadsheetFormat)
+
+def getDateFromPendulum():
+    return pendulum.now().to_date_string()
